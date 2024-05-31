3b97684f4448d7927e7ad0b5ce0f9777a00d42cc
Tamerlan <134862821+h3r0kun@users.noreply.github.com>
2024-05-31 14:37:56 +0500

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, jsonify
 from models import db, Workers
+from functools import wraps
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
+# registration
 @bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
@@ -21,7 +22,7 @@ def register():
     db.session.commit()
     return jsonify({"message": "Worker registered successfully"}), 201
 
-
+# change password
 @bp.route('/change-password', methods=['POST'])
 def change_password():
     data = request.get_json()
@@ -31,3 +32,19 @@ def change_password():
         db.session.commit()
         return jsonify({"message": "Password updated successfully"}), 200
     return jsonify({"message": "Invalid credentials"}), 400
+
+
+# basic authorization
+def basic_auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not auth.username or not auth.password:
+            return jsonify({"message": "Missing credentials"}), 401
+
+        worker = Workers.query.filter_by(login=auth.username).first()
+        if not worker or not worker.check_password(auth.password):
+            return jsonify({"message": "Invalid credentials"}), 401
+
+        return f(worker, *args, *kwargs)
+    return decorated
