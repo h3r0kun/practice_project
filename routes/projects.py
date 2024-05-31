846f2fae8ef1ee751d3650ffedb1980cd6abe22d
Tamerlan <134862821+h3r0kun@users.noreply.github.com>
2024-05-31 14:38:20 +0500

    Update projects.py

--- routes/projects.py
+++ routes/projects.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request, jsonify
 from models import db, Projects
+from routes.auth import basic_auth_required
+
 
 bp = Blueprint('projects', __name__, url_prefix='/projects')
 
+# creating project
 @bp.route('/', methods=['POST'])
+@basic_auth_required # checks authorization
 def create_project():
     data = request.get_json()
     project = Projects(
@@ -16,12 +20,16 @@ def create_project():
     db.session.commit()
     return jsonify({"message": "Project created successfully"}), 201
 
+# list of projects
 @bp.route('/', methods=['GET'])
+@basic_auth_required # and here
 def get_projects():
     projects = Projects.query.all()
     return jsonify([project.to_dict() for project in projects]), 200
 
+# updating project (duh)
 @bp.route('/<int:project_id>', methods=['PUT'])
+@basic_auth_required # and here too
 def update_project(project_id):
     data = request.get_json()
     project = Projects.query.get_or_404(project_id)
