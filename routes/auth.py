315984f5c6dfaf5ea150806ceff0007e3e12b763
Tamerlan <134862821+h3r0kun@users.noreply.github.com>
2024-06-10 13:22:13 +0500

    Update auth.py
    added use of schemas from schemas.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
 from models import db, Workers
 from functools import wraps
+from schemas import WorkerSchema
+from pydantic import ValidationError
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -8,16 +10,18 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    if 'login' not in data or 'password' not in data or 'email' not in data:
-        return jsonify({"message": "Missing fields"}), 400
+    try:
+        worker_data = WorkerSchema(**data)
+    except ValidationError as err:
+        return jsonify(err.errors()), 400
 
     worker = Workers(
-        name=data.get('name'),
-        title=data.get('title'),
-        login=data['login'],
-        email=data['email']
+        name=worker_data.name,
+        title=worker_data.title,
+        login=worker_data.login,
+        email=worker_data.email
     )
-    worker.set_password(data['password'])
+    worker.set_password(worker_data.password)
     db.session.add(worker)
     db.session.commit()
     return jsonify({"message": "Worker registered successfully"}), 201
@@ -33,7 +37,6 @@ def change_password():
         return jsonify({"message": "Password updated successfully"}), 200
     return jsonify({"message": "Invalid credentials"}), 400
 
-
 # basic authorization
 def basic_auth_required(f):
     @wraps(f)
@@ -46,5 +49,5 @@ def basic_auth_required(f):
         if not worker or not worker.check_password(auth.password):
             return jsonify({"message": "Invalid credentials"}), 401
 
-        return f(worker, *args, *kwargs)
+        return f(worker, *args, **kwargs)
     return decorated
