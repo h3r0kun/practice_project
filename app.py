07ffb1536ff23ab25b22d10296b37bdc55632fa8
Tamerlan <134862821+h3r0kun@users.noreply.github.com>
2024-06-10 13:47:57 +0500

    Update app.py
    Added error handler

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ from flask import Flask
 from config import Config
 from models import db
 from routes import auth, projects
+from errors import handle_http_exception, handle_validation_error, handle_generic_error
+from pydantic import ValidationError
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-# Регистрация блюпринтов
+# Register blueprints
 app.register_blueprint(auth.bp)
 app.register_blueprint(projects.bp)
 
-# app health check
+# Register error handlers
+app.register_error_handler(HTTPException, handle_http_exception)
+app.register_error_handler(ValidationError, handle_validation_error)
+app.register_error_handler(Exception, handle_generic_error)
+
+# Health check endpoint
 @app.route('/ping', methods=['GET'])
 def ping():
     return {"message": "pong"}, 200
